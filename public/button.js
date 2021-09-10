1a51427e6a5b3f1a1b630c569be7ca3ec41c13dd
Rustam <rsaifutdinov57@gmail.com>
2021-09-10 18:14:32 +0300

    homework les1and2

---
+++ public/button.js
@@ -0,0 +1,26 @@
+class Button{
+    _text=''
+    _onClickClb=null
+    constructor(text,clb) {
+        this._text=text
+        this._onClickClb=clb
+    }
+    onBtnClick(){
+        if(typeof this._onClickClb === 'function'){
+            this._onClickClb()
+        }
+    }
+    getMainTemplate(){
+        const btn=document.createElement('button')
+        btn.classList.add('btn')
+        return btn
+    }
+    getTemplate(){
+        const btn=this.getMainTemplate()
+        btn.innerHTML=this._text
+        btn.addEventListener('click',()=>{
+            this.onBtnClick()
+        })
+        return btn
+    }
+}
--- public/script.js
+++ public/script.js
@@ -1,6 +1,29 @@
-class List{
+class AbstractList{
     items=[]
-    constructor() {
+    constructor(item=[]) {
+        this.items=[]
+    }
+    add(item){
+        this.items.push(item)
+        this.render()
+    }
+    remove(){
+
+    }
+    render(){
+        this.items.forEach(good=>{
+            good.render()
+        })
+    }
+}
+
+
+class List extends AbstractList{
+    items=[]
+    _cartInstance=null
+    constructor(cartInstance) {
+        super()
+        this._cartInstance=cartInstance
         let goodsPromise=this.fetchGoods()
         goodsPromise.then(()=>{
             this.render()
@@ -13,59 +36,101 @@ class List{
             })
             .then(data=>{
                 this.items=data.data.map(cur=>{
-                    return new Good(cur)
+                    return new Good(cur,this._cartInstance)
                 })
             })
         return result
     }
     render(){
+        const placeToRender=document.querySelector('.goods-list')
+        if(placeToRender) {
+            placeToRender.innerHTML = ''
+            this.items.forEach(good => {
+                good.render(placeToRender)
+            })
+        }
+    }
+}
+
+class Cart extends AbstractList{
+    constructor() {
+        super()
+        this.init()
+    }
+    init(){
+        const block=document.createElement('div')
+        block.classList.add('cart')
+        const list=document.createElement('div')
+        list.classList.add('cart_list')
+        block.appendChild(list)
+        const placeToRender=document.querySelector('header')
+        const ButtonInstance= new Button('Корзина',()=>{
+            list.classList.toggle('shown')
+        })
+        block.appendChild(ButtonInstance.getTemplate())
+        if(placeToRender){
+            placeToRender.appendChild(block)
+        }
+    }
+    render(){
+        const placeToRender=document.querySelector('.cart_list')
+        placeToRender.innerHTML=''
         this.items.forEach(good=>{
-            good.render()
+            good.render(placeToRender)
         })
     }
 }
+
 class Good{
     name=''
     price=0
-    constructor({name,price}) {
+    _cartInstance=null
+    constructor({name,price},cartInstance=null) {
         this.name=name
         this.price=price
+        this._cartInstance=cartInstance
     }
-    render(){
-        const PlaceToRender = document.querySelector(".goods-list")
+    render(PlaceToRender){
         if(PlaceToRender){
-            const block = document.createElement("div")
-            block.innerHTML="Имя:"+this.name+" Цена:"+this.price
+            const block = document.createElement('div')
+            block.innerHTML=`
+        <div class="img">
+          <img src="https://ae01.alicdn.com/kf/UTB8CCmompPJXKJkSahVq6xyzFXaz.jpg" width="200px" height="200px"/>
+        </div>
+        <div class="meta">
+          <div class="meta__row">
+            <span class="key">Товар:</span>
+            <span class="value">${this.name}</span>
+          </div>
+          <div class="meta__row">
+            <span class="key">Цена:</span>
+            <span class="value">${this.price}</span>
+          </div>
+          <div class="btn_holder"></div>
+        </div>
+      `
             PlaceToRender.appendChild(block)
-            const BtnAdd=document.createElement("button")
-            const BtnRem=document.createElement("button")
-            block.appendChild(BtnAdd)
-            block.appendChild(BtnRem)
-            BtnAdd.innerHTML="+"
-            BtnRem.innerHTML="-"
+            const BtnAdd=new Button('Добавить в корзину',()=>{
+                this._cartInstance.add(new CartGood(this))
+            })
+            const BtnRem=document.createElement('button')
+            block.querySelector('.btn_holder').appendChild(BtnAdd.getTemplate())
         }
     }
-    addCart(){
-        const PlaceToRender = document.querySelector(".cart-list")
+}
+class CartGood extends Good{
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        const PlaceToRender=document.querySelector('.cart_list')
         if(PlaceToRender){
-            const block = document.createElement("div")
-            block.className=`${this.name}`
-            block.innerHTML="Имя:"+this.name+" Цена:"+this.price
+            const block =document.createElement('div')
+            block.innerHTML=`${this.name}=${this.price}`
             PlaceToRender.appendChild(block)
         }
     }
 }
-class Cart extends List{
+const CartInstance=new Cart()
+const ListInstance=new List(CartInstance)
 
-}
-class CartGood{
-
-}
-const ListInstance=new List()
-const BtnLoadMore=document.querySelector('.btn-load-more')
-BtnLoadMore.onclick=function loadmore(){
-    const good1=new Good({name:'Товар New', price:100})
-    const good2=new Good({name:'Товар New', price:100})
-    good1.render()
-    good2.render()
-}
